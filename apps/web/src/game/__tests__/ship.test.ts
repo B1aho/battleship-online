448e62702dac73cb5382f1c283cd5a6339d8a8a3
B1aho <wheelsurgut00@gmail.com>
2025-03-26 13:49:08 +0300

    Make test description more readable

--- apps/web/src/game/__tests__/ship.test.ts
+++ apps/web/src/game/__tests__/ship.test.ts
@@ -8,33 +8,33 @@ describe("Test Ship interface", () => {
             ship = new Ship();
         });
 
-        it("The ship should have method that return it length", () => {
+        it("Should contain method that return ship length", () => {
             expect(typeof ship.length).toBe("function");
         });
 
-        it("The ship should have method that that calculates whether a ship is considered sunk", () => {
+        it("Should contain method that that calculates whether a ship is considered sunk", () => {
             expect(typeof ship.isSunk).toBe("function");
         });
 
-        it("The ship should have method that increases the number of hits in ship", () => {
+        it("Should contain method that increases the number of hits in ship", () => {
             expect(typeof ship.hit).toBe("function");
         });
     });
 
-    describe("Test Ship instance creation", () => {
-        it("The ship should be as long as specified when the instance was created", () => {
+    describe("Test Ship length", () => {
+        it("Should be as long as specified when the instance was created", () => {
             const ship = new Ship(7);
             expect(ship.length()).toBe(7);
         });
 
-        it("The ship length can't be smaller then 1", () => {
+        it("Can't be smaller then 1", () => {
             expect(() => new Ship(-3)).toThrow("Ship's length must be >= 1");
             expect(() => new Ship(0)).toThrow("Ship's length must be >= 1");
         });
     });
 
-    describe("Should correct update hit count and determine if the ship is sunk", () => {
-        it("Check if ship hits equal length then ship sunk", () => {
+    describe("Test integration between hit() and isSunk()", () => {
+        it("Sould be sunk if ship got hits number equal ship's length", () => {
             const ship = new Ship(3);
             ship.hit();
             ship.hit();
@@ -43,7 +43,7 @@ describe("Test Ship interface", () => {
             expect(ship.isSunk()).toBe(true);
         });
 
-        it("Check if ship sunk when hits more then ship length", () => {
+        it("Should be sunk if ship got hits number greater then ship's length", () => {
             const ship = new Ship(1);
             ship.hit();
             ship.hit();
