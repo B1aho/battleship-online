495009cc4dc01d9dbfd5bb20db698cb4fbc27b71
B1aho <wheelsurgut00@gmail.com>
2025-03-28 05:17:13 +0300

    Test player class

--- apps/web/src/game/Player.ts
+++ apps/web/src/game/Player.ts
@@ -1,26 +1,26 @@
 import { Gameboard } from "./Gameboard";
 import { ICoord } from "./types";
 
-enum Connection {
+export enum Connection {
     ONLINE = "ONLINE",
     OFFLINE = "OFFLINE"
 }
 
 export class Player {
     name = "";
-    gameConnection = Connection.ONLINE;
+    #gameConnection = Connection.ONLINE;
     board: Gameboard;
 
     constructor(name: string, gameConnection = Connection.ONLINE) {
         this.name = name;
-        this.gameConnection = gameConnection;
+        this.#gameConnection = gameConnection;
         this.board = new Gameboard();
     }
 
-    // Метод для выполнения хода игроком (реальным или ИИ)
+    // Метод для выполнения хода реальным игроком 
     makeMove(): ICoord {
         // Логика хода реального игрока, например, выбор координат через UI
-        console.log(`${this.name} (человек) делает ход`);
+        console.log(`${this.name} make a move`);
         return { x: 0, y: 0 };
     }
 }
@@ -40,7 +40,7 @@ export class ComputerPlayer extends Player {
     // ИИ делает ход
     makeMove(): ICoord {
         const [x, y] = this.#getRandomMove();
-        console.log(`${this.name} (ИИ) делает ход на ${x}, ${y}`);
+        console.log(`${this.name} AI make a move ${x}, ${y}`);
         return { x, y };
     }
 }
---
+++ apps/web/src/game/__tests__/Player.test.ts
@@ -0,0 +1,17 @@
+import { Connection, Player } from "../Player";
+
+describe("Test Player class interface", () => {
+    let player: Player;
+    beforeEach(() => {
+        player = new Player("Patrik", Connection.OFFLINE);
+    });
+    it("should have gameboard", () => {
+        expect(player.board).not.toBe(undefined);
+        expect(player.board.getGrid()[0]![0])
+            .not.toBe(undefined);
+    });
+
+    it("should be able to make move", () => {
+        expect(typeof player.makeMove).toBe("function");
+    });
+});
