0ea3ad515cda0c8f2df69dd48db97a508d6b4a3a
B1aho <wheelsurgut00@gmail.com>
2025-03-29 18:35:08 +0300

    Add base layout and routes

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,13 +1,20 @@
+import { Route, Routes } from 'react-router';
 import './App.css'
-import { Board } from './features/GameBoard/Board';
-import { Gameboard } from './game/Gameboard';
+import { Home } from './pages/Home';
 
 function App() {
-  const game = new Gameboard();
-  game.placeRandom();
   return (
     <>
-      <Board gameboard={game} />
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index path="home" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route element={<AuthLayout />}>
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+          </Route>
+        </Route>
+      </Routes>
     </>
   )
 }
