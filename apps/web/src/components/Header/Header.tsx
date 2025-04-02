eaae75c19092516fa86944acfeeb445768225996
B1aho <wheelsurgut00@gmail.com>
2025-04-02 16:21:52 +0300

    Add comments and clean tailwind classes

--- apps/web/src/components/Header/Header.tsx
+++ apps/web/src/components/Header/Header.tsx
@@ -5,18 +5,18 @@ import { createRef, RefObject, useLayoutEffect, useMemo, useRef, useState } from
 import { useLinks } from "./links";
 import { useTranslation } from "react-i18next";
 
-
-
 export const Header = () => {
     const { t } = useTranslation("common");
     const links = useLinks();
+    // Reference to the marker that highlighting the active tab
     const markerRef = useRef<HTMLDivElement>(null);
+    // Mutable array of refs that refers to NavLinks elements
     const linkRefs = useRef<RefObject<HTMLAnchorElement | null>[]>([]);
+    // Active NavLink
     const [anchor, setAnchor] = useState<HTMLAnchorElement | null | undefined>(null);
-    // Флаг, отключающий transition при первой установке позиции
+    // Flag disabling transition on the first position setup
     const [disableTransition, setDisableTransition] = useState(true);
 
-    // Array of refs that refers to NavLinks
     linkRefs.current = useMemo(
         () => links.map(() => createRef<HTMLAnchorElement>()),
         [links]
@@ -25,29 +25,32 @@ export const Header = () => {
     useLayoutEffect(() => {
         const updateMarker = () => {
             if (anchor && markerRef.current) {
-                // При первом монтировании отключаем анимацию
+                // Disable animation on the first mount
                 markerRef.current.style.transition = disableTransition
                     ? "none"
                     : "left 400ms ease-in-out, width 400ms ease-in-out";
 
+                // Move along the X-axis to the active tab
                 markerRef.current.style.left = `${anchor.getBoundingClientRect().left + (anchor.getBoundingClientRect().width / 2)}px`;
-                //markerRef.current.style.width = `${anchor.offsetWidth}px`;
 
-                // Центрируем по оси Y
+                // Center along the Y-axis
                 markerRef.current.style.top = `${anchor.offsetTop + anchor.offsetHeight / 2}px`;
                 markerRef.current.style.transform = "translateY(-110%)"; // Центрируем
 
                 markerRef.current.style.opacity = "1";
-                // После первого вычисления позиций включаем transition
+                // Enable transition after the first position calculation
                 if (disableTransition) {
                     setDisableTransition(false);
                 }
             };
         };
 
+        // Update marker position on window resize
         window.addEventListener("resize", updateMarker);
+        // Set the marker on mount
         updateMarker();
 
+        // Cleanup the event handler
         return () => window.removeEventListener("resize", updateMarker);
     }, [anchor, disableTransition]);
 
@@ -55,13 +58,15 @@ export const Header = () => {
         <nav className="flex w-full h-[8rem] gap-2 relative items-center bg-gray-900 px-2 select-none text-white">
             <span className="flex gap-1 text-2xl justify-center items-center mr-4 z-10">
                 <Ship size={40} />
-                <span className="font-bold">{t("logo")}</span>
+                <span className="font-bold">
+                    {t("logo")}
+                </span>
             </span>
-            <ul className="flex items-center w-[80%] h-[60%] rounded-lg bg-gray-800 shadow-black shadow-[inset_0_1px_3px_rgba(0,0,0,0.1),inset_0_-1px_3px_rgba(0,0,0,0.1),inset_1px_0_3px_rgba(0,0,0,0.1),inset_-1px_0_3px_rgba(0,0,0,0.1)]">
+            <ul className="nav-container">
                 {
                     links.map((link, idx) => {
                         return (
-                            <li className="list-none text-lg w-[20%] h-full">
+                            <li key={`LI-${idx}`} className="list-none text-lg w-[20%] h-full">
                                 <NavLink className="flex items-center text-gray-500 z-10 group h-full relative" to={link.to} ref={linkRefs.current[idx]} key={link.key}>
                                     {({ isActive }) => {
                                         return (
@@ -79,10 +84,11 @@ export const Header = () => {
                     })
                 }
                 <div
-                    className="absolute opacity-0 top-[20%] left-[50%] -translate-x-1/2 w-[3.6rem] h-[50px] border-t-(--light-yellow) border-t-2 border-l-(--light-yellow) border-l-2 rounded-[15px] bg-(--dark-yellow) shadow-[0_0_0_6px_rgb(17,24,39),0_2px_0_5px_var(--light-yellow)] before:absolute before:top-[28%] before:-left-[32%] before:w-[10px] before:h-[10px] before:bg-transparent before:rounded-tr-lg before:shadow-[3px_-3px_0_2px_rgb(17,24,39)] after:absolute after:top-[28%] after:-right-[29%] after:w-[10px] after:h-[10px] after:bg-transparent after:rounded-tl-lg after:shadow-[-3px_-3px_0px_1px_rgb(17,24,39)]"
+                    className="nav-marker"
                     ref={markerRef}
-                    id="marker">
-                    <span className="absolute top-[74px] left-[50%] -translate-x-1/2 w-[7rem] h-[30px] bg-(--dark-yellow) rounded-[20px] border-t-(--light-yellow) border-t-2 shadow-[0_0_0_5px_rgb(17,24,39),0_-2px_0_4px_var(--light-yellow)]">
+                    id="marker"
+                >
+                    <span className="nav-marker-label">
                     </span>
                 </div>
             </ul>
